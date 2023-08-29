a743d910b798cec97d2ec780794e9ec0edd16f85
Kira Pitman <kirapitman@gmail.com>
2023-08-30 11:45:27 +1200

    read metadata of user

--- client/components/Nav.tsx
+++ client/components/Nav.tsx
@@ -5,19 +5,19 @@ import { useAuth0 } from '@auth0/auth0-react'
 function Nav() {
   // TODO: call the useAuth0 hook and destructure user, logout, and loginWithRedirect
   // TODO: replace placeholder user object with the one from auth0
-  const { logout, loginWithRedirect } = useAuth0()
+  const { logout, loginWithRedirect, user } = useAuth0()
 
-  const user = {
-    nickname: 'john.doe',
-  }
+  // const user = {
+  //   nickname: 'john.doe',
+  // }
 
   const handleSignOut = () => {
     logout()
-   
   }
 
   const handleSignIn = () => {
-     loginWithRedirect()
+    loginWithRedirect()
+    console.log(user)
   }
 
   return (
