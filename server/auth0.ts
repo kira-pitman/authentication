50a437a660236c789e4bfeeb3b55fb743cdf41cc
Kira Pitman <kirapitman@gmail.com>
2023-08-30 12:15:16 +1200

    set up jwt middleware

--- server/auth0.ts
+++ server/auth0.ts
@@ -5,8 +5,8 @@ import { JwtPayload } from 'jsonwebtoken'
 import jwks from 'jwks-rsa'
 
 // TODO: set the domain and audience (API Identifier)
-const domain = 'https://'
-const audience = 'https://'
+const domain = 'https://dev-2851w3ucmedk6h1w.au.auth0.com'
+const audience = 'https://fruits/api'
 
 const checkJwt = jwt({
   secret: jwks.expressJwtSecret({
