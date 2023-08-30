db984d621039577b50727dd1f3dce9647fc6b93f
Kira Pitman <kirapitman@gmail.com>
2023-08-30 12:13:49 +1200

    pass access tokens

--- client/components/Fruits.tsx
+++ client/components/Fruits.tsx
@@ -5,6 +5,9 @@ import SelectedFruitForm from './SelectedFruit.tsx'
 import AddFruitForm from './AddFruit.tsx'
 import { ErrorMessage } from './Styled.tsx'
 import { useFruits } from '../hooks.ts'
+import { useAuth0 } from '@auth0/auth0-react'
+
+
 
 type FormState =
   | {
@@ -23,6 +26,7 @@ function Fruits() {
     show: 'none',
   })
   const fruits = useFruits()
+ 
 
   const handleMutationSuccess = () => {
     handleCloseForm()
@@ -41,26 +45,28 @@ function Fruits() {
     onSuccess: handleMutationSuccess,
     onError: handleError,
   }
+  const { getAccessTokenSilently } = useAuth0()
 
-  const handleAdd = (fruit: FruitData) => {
+  const handleAdd = async (fruit: FruitData) => {
     // TODO: use getAccessTokenSilently to get an access token
-
+    const token = await getAccessTokenSilently(); 
+   
     // TODO: pass access token to mutate function
-    fruits.add.mutate({ fruit, token: 'token' }, mutationOptions)
+    fruits.add.mutate({ fruit, token: token }, mutationOptions)
   }
 
-  const handleUpdate = (fruit: Fruit) => {
+  const handleUpdate = async (fruit: Fruit) => {
     // TODO: use getAccessTokenSilently to get an access token
-
+    const token = await getAccessTokenSilently();
     // TODO: pass access token to mutate function
-    fruits.update.mutate({ fruit, token: 'token' }, mutationOptions)
+    fruits.update.mutate({ fruit, token: token }, mutationOptions)
   }
 
-  const handleDeleteFruit = (id: number) => {
+  const handleDeleteFruit = async (id: number) => {
     // TODO: use getAccessTokenSilently to get an access token
-
+    const token = await getAccessTokenSilently();
     // TODO: pass access token to mutate function
-    fruits.delete.mutate({ id, token: 'token' }, mutationOptions)
+    fruits.delete.mutate({ id, token: token }, mutationOptions)
   }
 
   const hideError = () => {
